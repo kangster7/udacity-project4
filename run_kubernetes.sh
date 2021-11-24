654e90df109337633d5e2a0e97b8f1ec8757ee34
EC2 Default User <ec2-user@ip-172-31-16-90.us-west-1.compute.internal>
2021-11-24 16:45:01 +0000

    updates

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -12,7 +12,9 @@ dockerpath=kangster7/project4
 # Step 2
 # Run the Docker Hub container with kubernetes
 
-kubectl create deployment udacityproject4 --image=$dockerpath
+kubectl run udacityproject4\
+    --image=$dockerpath\
+    --port=80 --labels app=udacityproject4
 
 # Step 3:
 # List kubernetes pods
@@ -23,4 +25,4 @@ sleep 5
 # Step 4:
 # Forward the container port to a host
 
-kubectl port-forward udacityproject4-5b6dfbd48f-csskh 8000:80
+kubectl port-forward udacityproject4 8000:80
